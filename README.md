1679de3e2ee2d66194569c173d7063f007ebbd48
Sam <samghelms@gmail.com>
2017-11-28 21:37:03 -0500

    dafd

--- README.md
+++ README.md
@@ -1,6 +1,22 @@
 # mathviz
-A python package for examining mathematics equation embeddings
+A python package for examining mathematics equation embeddings.
+
+Documentation: https://samghelms.github.io/mathviz/
 
 # Instructions
 
-install dependencies with pip and then run the jupyter notebook for an example of how to use the table tool.
+Install dependencies with pip and then run the jupyter notebook for an example of how to use the table tool.
+
+`pip install mathviz_hopper`
+
+## Building the docs
+
+### for local use
+Run `make html` in the `docs` directory
+
+### for github pages
+Run `make gh-pages` in the root directory of the repository
+
+## distributing 
+
+Run `setup.py ...`
--- mathviz_hopper/src/table.py
+++ mathviz_hopper/src/table.py
@@ -1,11 +1,11 @@
-'''
+"""
 Author: Sam Helms
 Date: Nov 1
 
-This class creates an html table that can display math
-equations.
+This module implements an interactive table that can display word embeddings.
+It can also render mathematics using KaTeX.
 
-'''
+"""
 import shutil
 import os
 import sys
@@ -18,48 +18,26 @@ from server import Server
 
 
 class Table:
-    """This function does something.
-
-    Args:
-       name (str):  The name to use.
-
-    Kwargs:
-       state (bool): Current state to be in.
-
-    Returns:
-       int.  The return code::
-
-          0 -- Success!
-          1 -- No good.
-          2 -- Try again.
-
-    Raises:
-       AttributeError, KeyError
-
-    A really great idea.  A way you might use me is
-
-    >>> print public_fn_with_googley_docstring(name='foo', state=None)
-    0
-
-    BTW, this always returns 0.  **NEVER** use with :class:`MyPublicClass`.
+    """
+    This class creates a server and a sets up an interactive 
+    javascript table for inspecting word embeddings.
 
     """
-    '''
-    @ param index: a lambda function that takes some input and returns k nearest neighbors
-    @ param converter: converts values to the format needed by the index 
-      (for example, it would convert a word to a vector)
-    @ param docs: a vector (list) of documents mapping to the index parameters returned by
-      the index function
-    @ writes a visualization to disk
-    '''
+
     def __init__(self, index, port = 8081):
-        '''
-        Constructor. Creates indices if passed data
-        @ param 
-            index: gensim index object (TODO: implement this for dataframes)
-            data: any data you want to 
-        '''
-        # TODO: make this memory efficient
+       """ Table Constructor
+        todo::make sure this is memory efficient
+        Args:
+           Index (Index):  An Index object with a valid .query method 
+                           and a .columns attribute.
+
+        Returns:
+           A table object
+
+        Usage example
+
+        >>> Table(ind)
+        """
         self.index = index
         self.server = None
         self.port = port if port else find_free_port()
@@ -71,27 +49,26 @@ class Table:
         # set to true if we want to delete the viz directory
         self.cleanup_flag = False
 
-        # path for the jupyter notebook TODO: change this to work with a package
-        # self.html_path = "/mathviz_hopper/src/html/build"
-
     def __del__(self):
+        """
+        Class destructor
+        """
         del self.server
 
         if self.cleanup_flag:
             shutil.rmtree('viz')
 
     def shutdown(self):
+        """
+        Shuts the server down
+        """
         del self.server
 
     def _create_settings(self):
-        '''
-        TODO: add this as another interface
-        Creates an index (can be used to initialize the index if not
-        passed in the consturctor)
-        @ param 
-            index: gensim index object (TODO: implement this for dataframes)
-            words: list of strings representing words in the vocabulary
-        '''
+        """ 
+        Creates the settings object that will be sent 
+        to the frontend vizualization
+        """
         self.settings = {
             "columns": [{"Header": s, "accessor": s} for s in self.settings],
             "port": self.port,
@@ -108,65 +85,44 @@ class Table:
         self._listen()
 
     def run_server(self):
-        '''
-        "prints" a javascript visualization that can be run in the browser
-        (or TODO: a jupyter notebook cell) and initializes listening on a
-        port to serve data to the viz.
-        '''
-        # _print_html()
-        # handler = TableRequestHandlerFactory(self.index, self.converter, self.docs, self.settings)
-        # self.server = HTTPServer(('', self.port), handler)
-        # self.server.serve_forever()
+        """ 
+        Runs a server to handle queries to the index without creating the 
+        javascript table.
+        """
         app = build_app()
         run(app, host='localhost', port=self.port)
 
     def print_ipython(self):
-        '''
-        "prints" a javascript visualization to a jupyter/ipython notebook cell
-        And initializes listening on a port to serve data to the viz.
-        '''
-        # _print_html()
+        """ 
+        Renders the javascript table to a jupyter/ipython notebook cell
+        Usage example:
+        >>> t = Table(ind)
+        >>> t.print_ipython()
+        ... renders the table to notebook cell
+        """
         from IPython.display import display, HTML
         self._listen()
         try: shutil.rmtree('viz')
         except: None
 
-        # TODO: assign this path intelligently
         shutil.copytree(self.html_path, 'viz')
         pth = "viz/index.html"
         html = open(pth).read()
         display(HTML(html))
 
-    def _query(self, q):
-        '''
-        Wraps gensim's query functionality. 
-        @ param q: a query string
-        @ returns list of nearest neighbor strings and their distances
-        '''
-
     def _listen(self):
-        '''
+        """
         Initializing listening on a socket to serve data to the javascript
         table.
-        '''
-        # TODO: write a method to deal with socket conflicts
+        """
         self.server = Server(self)
 
-        print 'listening'
-
     def _print_html(self):
-        '''
-        Creates an interactive table for viewing in the browser.
-        '''
+        """
+        Internal method to call the javascript/html table.
+        """
         cur_path = os.path.dirname(os.path.realpath(sys.argv[0]))
         shutil.copytree(cur_path+'webpage/template/', '')
-
-    def bundle(self):
-        '''
-        bundles the table and the model to a folder of javascript, html,
-        and css files that can be shared via email, github pages, or some
-        other service.
-        '''
 
 if __name__ == '__main__':
     import pandas as pd
